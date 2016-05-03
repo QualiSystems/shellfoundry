f5846e66f8f58a7e6568699bb89d8020dcf33d74
Boris Modylevsky <borismod@gmail.com>
2016-05-03 13:38:27 +0300

    shellfoundry references qpm

--- shellfoundry/__main__.py
+++ shellfoundry/__main__.py
@@ -1,9 +1,10 @@
-from cookiecutter import cli
+from qpm.packaging.auto_argument_parser import AutoArgumentParser
+from shellfoundry.gateway import ShellFoundryGateway
 
 
-def local_main():
-    cli.main()
-
+def main():
+    argument_parser = AutoArgumentParser(ShellFoundryGateway)
+    argument_parser.parse_args()
 
 if __name__ == "__main__":
-    local_main()
+    main()
