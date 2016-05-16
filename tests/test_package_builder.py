6afe0ee6a21fd5455f8cb39c99593985bd81a321
borismod <borismod@gmail.com>
2016-05-16 16:59:08 +0300

    test made cross platform

--- tests/test_package_builder.py
+++ tests/test_package_builder.py
@@ -10,19 +10,19 @@ class TestPackageBuilder(fake_filesystem_unittest.TestCase):
     # @unittest.skip('fake file system is not working as expected')
     def test_build_package_package_created(self):
         # Arrange
-        self.fs.CreateFile(u'C:\\work\\aws\\amazon_web_services\\datamodel\\datamodel.xml', contents='')
-        self.fs.CreateFile(u'C:\\work\\aws\\amazon_web_services\\datamodel\\shellconfig.xml', contents='')
-        self.fs.CreateFile(u'C:\\work\\aws\\amazon_web_services\\src\\driver.py', contents='')
+        self.fs.CreateFile('work/aws/amazon_web_services/datamodel/datamodel.xml', contents='')
+        self.fs.CreateFile('work/aws/amazon_web_services/datamodel/shellconfig.xml', contents='')
+        self.fs.CreateFile('work/aws/amazon_web_services/src/driver.py', contents='')
         builder = PackageBuilder()
 
         # Act
-        builder.build_package(u'C:\\work\\aws\\amazon_web_services', 'aws')
+        builder.build_package('work/aws/amazon_web_services', 'aws')
 
         # Assert
-        self.assertFileExists(u'C:\\work\\aws\\amazon_web_services\\package\\datamodel\datamodel.xml')
-        self.assertFileExists(u'C:\\work\\aws\\amazon_web_services\\package\\Configuration\shellconfig.xml')
-        self.assertFileExists(u'C:\\work\\aws\\amazon_web_services\\package\\Resource Drivers - Python\\aws Driver.zip')
-        self.assertFileExists(u'C:\\work\\aws\\amazon_web_services\\aws.zip')
+        self.assertFileExists('work/aws/amazon_web_services/package/datamodel/datamodel.xml')
+        self.assertFileExists('work/aws/amazon_web_services/package/Configuration/shellconfig.xml')
+        self.assertFileExists('work/aws/amazon_web_services/package/Resource Drivers - Python/aws Driver.zip')
+        self.assertFileExists('work/aws/amazon_web_services/aws.zip')
 
     def assertFileExists(self, file_path):
         self.assertTrue(os.path.exists(file_path), msg='File {0} does not exist'.format(file_path))
