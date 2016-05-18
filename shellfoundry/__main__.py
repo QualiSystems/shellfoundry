ad75d9204b3bd36176805f7f193fd9302aae6566
borismod <borismod@gmail.com>
2016-05-18 10:31:04 +0300

    usage of CLI was improved

--- shellfoundry/__main__.py
+++ shellfoundry/__main__.py
@@ -2,55 +2,52 @@ import os
 import click
 import pkg_resources
 from cookiecutter.main import cookiecutter
-from shellfoundry.config_reader import ConfigReader
 from shellfoundry.installer import ShellInstaller
 from shellfoundry.package_builder import PackageBuilder
 from shellfoundry.template_retriever import TemplateRetriever
+from shellfoundry.config_reader import ConfigReader
 
 
 @click.group()
 def cli():
-    click.echo('shellfoundry, version ' + pkg_resources.get_distribution("qpm").version)
+    click.echo(u'shellfoundry - CloudShell shell command-line tool')
     pass
 
 
 @cli.command()
 def version():
     """
-    Prints the current version of shellfoundry
-    :return:
+    Show shellfoundry version.
     """
-    pass
+    click.echo(u'Version: ' + pkg_resources.get_distribution(u'shellfoundry').version)
 
 
 @cli.command()
 def list():
     """
-    Lists CloudShell shell templates
-    :return:
+    List shell templates.
     """
     template_retriever = TemplateRetriever()
     templates = template_retriever.get_templates()
-    click.echo('Supported templates are: \r\n {0}'.format(_get_templates_with_comma(templates)))
+    click.echo(u'Supported templates are: \r\n {0}'.format(_get_templates_with_comma(templates)))
 
 
 @cli.command()
-@click.argument(u'template')
-def new(template):
+@click.option(u'--name', prompt=u'Type shell name:', help=u'Shell name to be created.')
+@click.option(u'--template', default=u'default', help=u'Shell template to be used.')
+def new(name, template):
     """
-    Create a new CloudShell shell based on a template
-    :param template: CloudShell shell template to be used.
-    :return:
+    Create a new shell based on a template.
     """
     template_retriever = TemplateRetriever()
     templates = template_retriever.get_templates()
 
     if template not in templates:
         raise click.BadParameter(
-            'Template {0} does not exist. Supported templates are: {1}'.format(template,
-                                                                               _get_templates_with_comma(templates)))
+            u'Template {0} does not exist. Supported templates are: {1}'.format(template,
+                                                                                _get_templates_with_comma(templates)))
 
-    cookiecutter(templates[template])
+    cookiecutter(templates[template], no_input=True, extra_context={u'project_name': name})
 
 
 def _get_templates_with_comma(templates):
@@ -58,26 +55,22 @@ def _get_templates_with_comma(templates):
 
 
 @cli.command()
-@click.option(u'--path', default=None)
-def build(path):
+def pack():
     """
-    Builds a CloudShell package
-    :param path: Path to the source directory
-    :return:
+    Pack the shell package.
     """
     config_reader = ConfigReader()
     package_builder = PackageBuilder()
 
     project = config_reader.read()
-    current_path = path or os.getcwd()
+    current_path = os.getcwd()
     package_builder.build_package(current_path, project.name)
 
 
 @cli.command()
 def install():
     """
-    Installs a CloudShell shell into CloudShell
-    :return:
+    Install the shell package into CloudShell.
     """
     config_reader = ConfigReader()
     installer = ShellInstaller()
