6317f938dd4c33ab37c79426b6e163d8a21f23b9
borismod <borismod@gmail.com>
2016-05-22 14:59:23 +0300

    test_package_builder was fixed to be cross platform

--- tests/test_utilities/test_package_builder.py
+++ tests/test_utilities/test_package_builder.py
@@ -9,20 +9,20 @@ class TestPackageBuilder(fake_filesystem_unittest.TestCase):
 
     def test_build_package_package_created(self):
         # Arrange
-        self.fs.CreateFile('\\work\\aws\\amazon_web_services\\datamodel\\datamodel.xml', contents='')
-        self.fs.CreateFile('\\work\\aws\\amazon_web_services\\datamodel\\shellconfig.xml', contents='')
-        self.fs.CreateFile('\\work\\aws\\amazon_web_services\\src\\driver.py', contents='')
+        self.fs.CreateFile('work/aws/amazon_web_services/datamodel/datamodel.xml', contents='')
+        self.fs.CreateFile('work/aws/amazon_web_services/datamodel/shellconfig.xml', contents='')
+        self.fs.CreateFile('work/aws/amazon_web_services/src/driver.py', contents='')
         os.chdir('work')
         builder = PackageBuilder()
 
         # Act
-        builder.build_package('\\work\\aws\\amazon_web_services', 'aws')
+        builder.build_package('aws/amazon_web_services', 'aws')
 
         # Assert
-        assertFileExists(self, '\\work\\aws\\amazon_web_services\\package\\datamodel\\datamodel.xml')
-        assertFileExists(self, '\\work\\aws\\amazon_web_services\\package\\Configuration\\shellconfig.xml')
-        assertFileExists(self, '\\work\\aws\\amazon_web_services\\package/Resource Drivers - Python\\aws Driver.zip')
-        assertFileExists(self, '\\work\\aws\\amazon_web_services\\dist\\aws.zip')
+        assertFileExists(self, 'aws/amazon_web_services/package/datamodel/datamodel.xml')
+        assertFileExists(self, 'aws/amazon_web_services/package/Configuration/shellconfig.xml')
+        assertFileExists(self, 'aws/amazon_web_services/package/Resource Drivers - Python/aws Driver.zip')
+        assertFileExists(self, 'aws/amazon_web_services/dist/aws.zip')
 
 
 
