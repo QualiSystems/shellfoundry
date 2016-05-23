c1ce01c05732f3bcd97d6f2e54c3c9a3199b637c
Boris Modylevsky <borismod@gmail.com>
2016-05-23 09:40:27 +0300

    displaying template description in list command

--- tests/test_commands/test_list_command.py
+++ tests/test_commands/test_list_command.py
@@ -1,18 +1,15 @@
 import unittest
-
 from mock import Mock, patch
-
 from shellfoundry.commands.list_command import ListCommandExecutor
 from shellfoundry.models.shell_template import ShellTemplate
 
 
 class TestListCommand(unittest.TestCase):
-
     @patch('click.echo')
-    def test_list_command_does_not_fail(self, echo_mock):
+    def test_single_template_is_displayed(self, echo_mock):
         # Arrange
         template_retriever = Mock()
-        template_retriever.get_templates = Mock(return_value={'base': ShellTemplate('base', '', '')})
+        template_retriever.get_templates = Mock(return_value={'base': ShellTemplate('base', 'description', '')})
 
         list_command_executor = ListCommandExecutor(template_retriever)
 
@@ -20,4 +17,21 @@ class TestListCommand(unittest.TestCase):
         list_command_executor.list()
 
         # Assert
-        echo_mock.assert_called_once_with(u'Supported templates are: \r\n base')
+        echo_mock.assert_called_once_with(u'Supported templates are:\r\nbase: description')
+
+    @patch('click.echo')
+    def test_two_templates_are_displayed(self, echo_mock):
+        # Arrange
+        template_retriever = Mock()
+        template_retriever.get_templates = Mock(return_value={'base': ShellTemplate('base', 'base description', ''),
+                                                              'switch': ShellTemplate('switch', 'switch description',
+                                                                                      '')})
+
+        list_command_executor = ListCommandExecutor(template_retriever)
+
+        # Act
+        list_command_executor.list()
+
+        # Assert
+        echo_mock.assert_called_once_with(
+            u'Supported templates are:\r\nswitch: switch description\r\nbase: base description')
