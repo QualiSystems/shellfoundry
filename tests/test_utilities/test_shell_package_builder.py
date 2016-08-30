bdf0e1b348d2f94504bd69668e63f8a741182f03
Boris Modylevsky <borismod@gmail.com>
2016-08-30 13:04:00 +0300

    add tests for shell_package_builder to improve coverage

--- tests/test_utilities/test_shell_package_builder.py
+++ tests/test_utilities/test_shell_package_builder.py
@@ -52,3 +52,73 @@ class TestShellPackageBuilder(fake_filesystem_unittest.TestCase):
         assertFileExists(self, 'dist/package_content/nxos.png')
         assertFileExists(self, 'dist/package_content/NutShellDriver.zip')
 
+    def test_tosca_based_shell_packed_when_artifacts_missing_in_yaml_file(self):
+        # Arrange
+        self.fs.CreateFile('nut-shell/TOSCA-Metadata/TOSCA.meta',
+                           contents='TOSCA-Meta-File-Version: 1.0 \n'
+                                    'CSAR-Version: 1.1 \n'
+                                    'Created-By: Anonymous\n'
+                                    'Entry-Definitions: shell-definition.yml')
+
+        self.fs.CreateFile('nut-shell/shell-definition.yml',
+                           contents='tosca_definitions_version: tosca_simple_yaml_1_0\n'
+                                    'metadata:\n'
+                                    '  template_name: NutShell\n'
+                                    '  template_author: Anonymous\n'
+                                    '  template_version: 1.0.0\n'
+                                    'node_types:\n'
+                                    '  vendor.switch.NXOS:\n'
+                                    '    derived_from: cloudshell.nodes.Switch')
+
+        os.chdir('nut-shell')
+
+        shell_package_builder = ShellPackageBuilder()
+
+        # Act
+        shell_package_builder.pack('nut-shell')
+
+        # Assert
+        assertFileExists(self, 'dist/nut-shell.zip')
+        TestPackageBuilder.unzip('dist/nut-shell.zip', 'dist/package_content')
+
+        assertFileExists(self, 'dist/package_content/TOSCA-Metadata/TOSCA.meta')
+        assertFileExists(self, 'dist/package_content/shell-definition.yml')
+        assertFileExists(self, 'dist/package_content/NutShellDriver.zip')
+
+    def test_tosca_based_shell_packed_when_some_artifacts_missing_in_directory(self):
+        # Arrange
+        self.fs.CreateFile('nut-shell/TOSCA-Metadata/TOSCA.meta',
+                           contents='TOSCA-Meta-File-Version: 1.0 \n'
+                                    'CSAR-Version: 1.1 \n'
+                                    'Created-By: Anonymous\n'
+                                    'Entry-Definitions: shell-definition.yml')
+
+        self.fs.CreateFile('nut-shell/shell-definition.yml',
+                           contents='tosca_definitions_version: tosca_simple_yaml_1_0\n'
+                                    'metadata:\n'
+                                    '  template_name: NutShell\n'
+                                    '  template_author: Anonymous\n'
+                                    '  template_version: 1.0.0\n'
+                                    'node_types:\n'
+                                    '  vendor.switch.NXOS:\n'
+                                    '    derived_from: cloudshell.nodes.Switch\n'
+                                    '    artifacts:\n'
+                                    '      icon:\n'
+                                    '        file: nxos.png\n'
+                                    '        type: tosca.artifacts.File\n')
+
+        os.chdir('nut-shell')
+
+        shell_package_builder = ShellPackageBuilder()
+
+        # Act
+        shell_package_builder.pack('nut-shell')
+
+        # Assert
+        assertFileExists(self, 'dist/nut-shell.zip')
+        TestPackageBuilder.unzip('dist/nut-shell.zip', 'dist/package_content')
+
+        assertFileExists(self, 'dist/package_content/TOSCA-Metadata/TOSCA.meta')
+        assertFileExists(self, 'dist/package_content/shell-definition.yml')
+        assertFileExists(self, 'dist/package_content/NutShellDriver.zip')
+
