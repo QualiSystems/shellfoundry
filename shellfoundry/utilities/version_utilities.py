cf55ab47244c2275438223544452a3dc132499f8
kalsky <yanivka@gmail.com>
2016-08-30 15:04:42 -0700

    fix version_utilities

--- shellfoundry/utilities/version_utilities.py
+++ shellfoundry/utilities/version_utilities.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 class VersionUtilities(object):
     @staticmethod
+    def get_timestamped_build_and_revision():
         days = (datetime.utcnow() - datetime(2000, 1, 1)).days
         now = datetime.now()
         seconds_since_midnight = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
