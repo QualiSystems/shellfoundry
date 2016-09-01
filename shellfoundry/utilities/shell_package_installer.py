7c37df60f438285f15410ef32e3fcd31b22f99a8
Boris Modylevsky <borismod@gmail.com>
2016-09-01 10:39:29 +0300

    upgrade cloudshell-rest-api to 7.2.0.6

--- shellfoundry/utilities/shell_package_installer.py
+++ shellfoundry/utilities/shell_package_installer.py
@@ -4,7 +4,7 @@ from cloudshell.rest.exceptions import ShellNotFoundException
 
 from shellfoundry.utilities.config_reader import CloudShellConfigReader
 from shellfoundry.utilities.shell_package import ShellPackage
-from cloudshell.rest.api import CloudShellRestApiClient
+from cloudshell.rest.api import PackagingRestApiClient
 
 
 class ShellPackageInstaller(object):
@@ -20,11 +20,11 @@ class ShellPackageInstaller(object):
 
         click.echo('Connecting to CloudShell {0}'.format(cloudshell_config.host))
 
-        client = CloudShellRestApiClient(ip=cloudshell_config.host,
-                                         username=cloudshell_config.username,
-                                         port=cloudshell_config.port,
-                                         domain=cloudshell_config.domain,
-                                         password=cloudshell_config.password)
+        client = PackagingRestApiClient(ip=cloudshell_config.host,
+                                        username=cloudshell_config.username,
+                                        port=cloudshell_config.port,
+                                        domain=cloudshell_config.domain,
+                                        password=cloudshell_config.password)
 
         try:
             click.echo('Updating shell {0}'.format(package_full_path))
