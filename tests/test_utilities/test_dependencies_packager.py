57d72b484d571ac4e35772dcd3219e259bab42c8
Boris Modylevsky <borismod@gmail.com>
2016-09-14 14:35:01 +0300

    add test test_calls_pip_to_download_dependencies_with_proxy to improve code coverage

--- tests/test_utilities/test_dependencies_packager.py
+++ tests/test_utilities/test_dependencies_packager.py
@@ -30,6 +30,26 @@ class TestPythonDependenciesPackager(fake_filesystem_unittest.TestCase):
             self.assertIn('--requirement=req/requirements.requirements_file', main_params)
             self.assertIn('--dest=dst', main_params)
 
+    def test_calls_pip_to_download_dependencies_with_proxy(self):
+        with patch('shellfoundry.utilities.python_dependencies_packager.os.environ.get') as environ_mock:
+            environ_mock.return_value = 'HTTP PROXY'
+
+            requirements_file = 'req/requirements.requirements_file'
+            self.fs.CreateFile(requirements_file, contents="")
+
+            with patch('shellfoundry.utilities.python_dependencies_packager.pip.main') as mock_pip:
+                # Act
+                packager = PythonDependenciesPackager()
+                packager.save_offline_dependencies(requirements_file, 'dst')
+                self.assertTrue(mock_pip.called, 'pip.main should be called')
+                _, call_params, _ = mock_pip.mock_calls[0]
+                main_params = call_params[0]
+                self.assertIn('download', main_params)
+                self.assertIn('--requirement=req/requirements.requirements_file', main_params)
+                self.assertIn('--dest=dst', main_params)
+                self.assertIn('--proxy', main_params)
+                self.assertIn('HTTP PROXY', main_params)
+
     def test_removed_old_files_before_running(self):
         requirements_file = 'req/requirements.requirements_file'
         self.fs.CreateFile(requirements_file, contents="")
