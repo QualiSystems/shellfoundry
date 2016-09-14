acc411321571431c4d63ba0a3fa95b490a7cb5ee
Boris Modylevsky <borismod@gmail.com>
2016-09-14 12:51:42 +0300

    improve code coverage

--- tests/test_utilities/test_driver_generator.py
+++ tests/test_utilities/test_driver_generator.py
@@ -1,11 +1,11 @@
 # coding=utf-8
-from mock import patch, Mock
+from mock import patch, Mock, MagicMock
 from pyfakefs import fake_filesystem_unittest
 
 from shellfoundry.models.install_config import InstallConfig
 from shellfoundry.utilities.archive_creator import ArchiveCreator
 from shellfoundry.utilities.driver_generator import DriverGenerator
-from tests.asserts import assertFileExists
+from tests.asserts import assertFileExists, assertFileDoesNotExist
 
 
 class TestDriverGenerator(fake_filesystem_unittest.TestCase):
@@ -46,3 +46,40 @@ class TestDriverGenerator(fake_filesystem_unittest.TestCase):
 
         # Assert
         assertFileExists(self, 'nut-shell/src/data_model.py')
+
+    def test_error_displayed_when_driver_generation_returns_error_code(self):
+        self.fs.CreateFile('nut-shell/dist/NutShell.zip', contents='ZIP')
+
+        self.fs.CreateFile('nut-shell/temp/data_model.py', contents='python data model content')
+
+        ArchiveCreator.make_archive('nut-shell/temp/data-model', 'zip', 'nut-shell/temp')
+
+        driver_generator = DriverGenerator()
+        config = InstallConfig('TEST-HOST', 9000, 'user', 'pwd', 'Global')
+
+        with patch('shellfoundry.utilities.driver_generator.PackagingRestApiClient') as mock_rest:
+            rest_client_mock = Mock()
+            rest_client_mock.token = 'TEST-TOKEN'
+            mock_rest.return_value = rest_client_mock
+
+            with patch('shellfoundry.utilities.driver_generator.post') as post_mock:
+                response = Mock()
+                response.status_code = 500
+                response.content = 'Error occurred'
+                post_mock.return_value = response
+
+                with patch('shellfoundry.utilities.driver_generator.click') as click_mock:
+                    click_mock.echo = MagicMock()
+
+                    # Act
+                    driver_generator.generate_driver(cloudshell_config=config,
+                                                     destination_path='nut-shell/src',
+                                                     package_full_path='nut-shell/dist/NutShell.zip',
+                                                     shell_filename='NutShell.zip',
+                                                     shell_name='NutShell')
+
+                self.assertTrue(click_mock.echo.called, 'click should have been called')
+
+        # Assert
+        assertFileDoesNotExist(self, 'nut-shell/src/data_model.py')
+
