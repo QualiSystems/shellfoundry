db672d6a445af6aba1f857856a4cf38bceeb02cb
johnathanvidu <johnathan.vidu@gmail.com>
2017-06-26 17:57:04 +0300

    new command now abort the app if there is a new major version on pypi

--- tests/test_decorators/test_version_check.py
+++ tests/test_decorators/test_version_check.py
@@ -4,11 +4,17 @@ import unittest
 import StringIO
 
 from mock import patch
+from click import Abort
 from shellfoundry.decorators import shellfoundry_version_check
 
 
-@shellfoundry_version_check
-def test_me():
+@shellfoundry_version_check(abort_if_major=False)
+def test_me_do_not_abort():
+    print('vido', end='')
+
+
+@shellfoundry_version_check(abort_if_major=True)
+def test_me_abort_if_major():
     print('vido', end='')
 
 
@@ -18,7 +24,7 @@ class TestShellFoundryVersionCheck(unittest.TestCase):
         # Act
         with patch('shellfoundry.utilities.get_installed_version', return_value='5.0.0'), \
              patch('shellfoundry.utilities.max_version_from_index', return_value='0.2.9'):
-            test_me()
+            test_me_do_not_abort()
 
         # Assert
         self.assertEqual(stdout_mock.getvalue(), 'vido')
@@ -28,7 +34,7 @@ class TestShellFoundryVersionCheck(unittest.TestCase):
         # Act
         with patch('shellfoundry.utilities.get_installed_version', return_value='0.2.7'), \
              patch('shellfoundry.utilities.max_version_from_index', return_value='0.3.0'):
-            test_me()
+            test_me_do_not_abort()
 
         # Assert
         self.assertEqual(stdout_mock.getvalue(),
@@ -39,19 +45,29 @@ class TestShellFoundryVersionCheck(unittest.TestCase):
         # Act
         with patch('shellfoundry.utilities.get_installed_version', return_value='0.2.7'), \
              patch('shellfoundry.utilities.max_version_from_index', return_value='1.0.0'):
-            test_me()
+            test_me_do_not_abort()
 
         # Assert
         self.assertEqual(stdout_mock.getvalue(),
                          'vido\nThis version of shellfoundry is not supported anymore, please upgrade by running: pip install shellfoundry --upgrade\n')
-
 
     @patch('sys.stdout', new_callable=StringIO.StringIO)
     def test_version_check_when_current_is_equal_to_index(self, stdout_mock):
         # Act
         with patch('shellfoundry.utilities.get_installed_version', return_value='1.0.0'), \
              patch('shellfoundry.utilities.max_version_from_index', return_value='1.0.0'):
-            test_me()
+            test_me_do_not_abort()
 
         # Assert
         self.assertEqual(stdout_mock.getvalue(), 'vido')
+
+    @patch('sys.stdout', new_callable=StringIO.StringIO)
+    def test_version_check_when_current_is_lower_than_index_and_its_major_version_validate_abort(self, stdout_mock):
+        # Act
+        with patch('shellfoundry.utilities.get_installed_version', return_value='0.2.7'), \
+             patch('shellfoundry.utilities.max_version_from_index', return_value='1.0.0'):
+            self.assertRaises(Abort, test_me_abort_if_major)
+
+        # Assert
+        self.assertEqual(stdout_mock.getvalue(),
+                         'This version of shellfoundry is not supported anymore, please upgrade by running: pip install shellfoundry --upgrade\n\n')
