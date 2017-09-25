8e428f8d975fbca9d3597e4d368b57d82739642d
alexquali <alexey.b@quali.com>
2017-09-25 13:54:13 +0300

    Added author field to shellfoundry configuration

--- shellfoundry/utilities/cookiecutter_integration.py
+++ shellfoundry/utilities/cookiecutter_integration.py
@@ -1,14 +1,24 @@
-from cookiecutter.main import cookiecutter
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import os
 
+from cookiecutter.main import cookiecutter
+from shellfoundry.commands.config_command import ConfigCommandExecutor
+from shellfoundry.utilities.config_reader import Configuration, AUTHOR
 
-class CookiecutterTemplateCompiler:
+
+class CookiecutterTemplateCompiler(object):
     def __init__(self):
         pass
 
     def compile_template(self, shell_name, template_path, extra_context, running_on_same_folder):
 
-        extra_context['project_name'] = shell_name
+        extra_context["project_name"] = shell_name
+        config_path = ConfigCommandExecutor._get_config_file_path(is_global_flag=True)
+        extra_context["full_name"] = Configuration.get_configuration_attr(config_path=config_path,
+                                                                          attr_name=AUTHOR)
+
         if running_on_same_folder:
             output_dir = os.path.pardir
         else:
