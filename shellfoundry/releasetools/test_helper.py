2077fb4fa06e3f4fec6c0f247207d462959ba802
Yoram Shamir <yoram@ignissoft.com>
2020-01-26 10:56:19 +0100

    refactor after health check project

--- shellfoundry/releasetools/test_helper.py
+++ shellfoundry/releasetools/test_helper.py
@@ -1,4 +1,5 @@
 import inspect
+import os
 from os import path
 import time
 import yaml
@@ -7,7 +8,13 @@ import xml.etree.ElementTree as ET
 from cloudshell.shell.core.driver_context import (ResourceCommandContext, ResourceContextDetails,
                                                   ReservationContextDetails, ConnectivityContext, InitCommandContext)
 from cloudshell.api.cloudshell_api import CloudShellAPISession, ResourceAttributesUpdateRequest
+from cloudshell.api.common_cloudshell_api import CloudShellAPIError
 from cloudshell.shell.core.session.cloudshell_session import CloudShellSessionContext
+
+from cloudshell.helpers.scripts.cloudshell_dev_helpers import attach_to_cloudshell_as
+import cloudshell.helpers.scripts.cloudshell_scripts_helpers as script_help
+
+
 
 
 #
@@ -31,40 +38,167 @@ def create_session_from_cloudshell_config():
 # 2nd generation helers.
 #
 
+def get_deployment_root():
 
-def get_namespace_from_cloudshell_config():
+    index = 1
+    test_name = inspect.stack()[index][1]
+    while test_name == __file__:
+        index += 1
+        test_name = inspect.stack()[index][1]
+    shell_dir = path.dirname(path.dirname(test_name))
+    if not path.exists(shell_dir + '/deployment.xml'):
+        shell_dir = path.dirname(shell_dir)
+    deployment = shell_dir + '/deployment.xml'
+    return ET.parse(deployment).getroot()
 
-    test_name = inspect.stack()[1][1]
-    f = path.join(path.dirname(path.dirname(test_name)), 'shell-definition.yaml')
-    with open(f, 'r') as f:
-        doc = yaml.load(f)
-    return list(doc['node_types'].keys())[0].split('.')[-1]
 
+def get_credentials_from_deployment():
 
-def create_session_from_deployment():
-
-    test_name = inspect.stack()[1][1]
-    deployment = path.join(path.dirname(path.dirname(test_name)), 'deployment.xml')
-    root = ET.parse(deployment).getroot()
+    root = get_deployment_root()
     host = root.find('serverRootAddress').text
     username = root.find('username').text
     password = root.find('password').text
     domain = root.find('domain').text
+    return host, username, password, domain
 
-    return CloudShellAPISession(host, username, password, domain)
+
+def create_topology_reservation(session, topology_path, reservation_name='tg regression tests', global_inputs=[]):
+
+    _, owner, _, _ = get_credentials_from_deployment()
+    try:
+        reservation = session.CreateImmediateTopologyReservation(reservationName=reservation_name,
+                                                                 topologyFullPath=topology_path,
+                                                                 globalInputs=global_inputs,
+                                                                 owner=owner, durationInMinutes=60)
+    except CloudShellAPIError as e:
+        reservations = session.GetCurrentReservations(reservationOwner='admin')
+        for reservation in reservations.Reservations:
+            if reservation.Name == 'tg regression tests':
+                session.EndReservation(reservation.Id)
+        time.sleep(2)
+        reservation = session.CreateImmediateTopologyReservation(reservationName=reservation_name,
+                                                                 topologyFullPath=topology_path,
+                                                                 globalInputs=global_inputs,
+                                                                 owner=owner, durationInMinutes=60)
+    return reservation
 
 
-def create_autoload_context_2g(session, model, address, attributes):
+def create_reservation(session, reservation_name='tg regression tests'):
+
+    _, owner, _, _ = get_credentials_from_deployment()
+    reservations = session.GetCurrentReservations(reservationOwner=owner)
+    for reservation in [r for r in reservations.Reservations if r.Name == reservation_name]:
+        session.EndReservation(reservation.Id)
+    reservation = session.CreateImmediateReservation(reservationName=reservation_name, owner=owner,
+                                                     durationInMinutes=60)
+    return reservation
+
+
+def create_session_from_deployment():
+    return CloudShellAPISession(*get_credentials_from_deployment())
+
+
+def end_reservation(session, reservation_id):
+    try:
+        session.EndReservation(reservation_id)
+        while session.GetReservationDetails(reservation_id).ReservationDescription.Status != 'Completed':
+            time.sleep(1)
+        session.DeleteReservation(reservation_id)
+    except Exception as _:
+        pass
+
+
+def create_init_command_context(session, family, model, address, attributes, type):
+    """
+    :param CloudShellAPISession session:
+    :param family:
+    :param model:
+    :param address:
+    :param type: Resource or Service
+    :param attributes:
+    :return: Dict of attributes
+    """
+    connectivity = ConnectivityContext(session.host, '8029', '9000', session.token_id, '9.1',
+                                       CloudShellSessionContext.DEFAULT_API_SCHEME)
+    resource = ResourceContextDetails(id='ididid', name='testing', fullname='Testing/testing',
+                                      address=address, family=family, model=model, attributes=attributes, type=type,
+                                      app_context='', networks_info='', description='',
+                                      shell_standard='', shell_standard_version='')
+    context = InitCommandContext(connectivity, resource)
+    return context
+
+
+def create_service_command_context(session, service_name, alias=None, attributes=[]):
+    """
+
+    :param CloudShellAPISession session:
+    :param service_name:
+    :param alias:
+    :param attributes:
+    :return:
+    """
+    reservation = create_reservation(session)
+    reservation_id = reservation.Reservation.Id
+    if not alias:
+        alias = service_name
+    session.AddServiceToReservation(reservationId=reservation_id,
+                                    serviceName=service_name,
+                                    alias=alias,
+                                    attributes=attributes)
+
+    os.environ['DEVBOOTSTRAP'] = 'True'
+    debug_attach_from_deployment(reservation_id, service_name=service_name)
+    reservation = script_help.get_reservation_context_details()
+    resource = script_help.get_resource_context_details()
 
     connectivity = ConnectivityContext(session.host, '8029', '9000', session.token_id, '9.1',
                                        CloudShellSessionContext.DEFAULT_API_SCHEME)
-    resource = ResourceContextDetails(id='ididid', name='testing', fullname='Testing/testing', type='Resource', address=address,
-                                      model=model, family='CS_TrafficGeneratorChassis', description='',
-                                      attributes=attributes, app_context='', networks_info='',
-                                      shell_standard='cloudshell_traffic_generator_chassis_standard',
-                                      shell_standard_version='1_0_3')
-    context = InitCommandContext(connectivity, resource)
+
+    context = ResourceCommandContext(connectivity, resource, reservation, [])
     return context
+
+
+def create_resource_command_context(session, resource_path):
+
+    reservation = create_reservation(session)
+    reservation_id = reservation.Reservation.Id
+    session.AddResourcesToReservation(reservationId=reservation_id, resourcesFullPath=[resource_path])
+
+    os.environ['DEVBOOTSTRAP'] = 'True'
+    debug_attach_from_deployment(reservation_id, resource_path)
+    reservation = script_help.get_reservation_context_details()
+    resource = script_help.get_resource_context_details()
+
+    connectivity = ConnectivityContext(session.host, '8029', '9000', session.token_id, '9.1',
+                                       CloudShellSessionContext.DEFAULT_API_SCHEME)
+
+    context = ResourceCommandContext(connectivity, resource, reservation, [])
+    return context
+
+
+def create_autoload_resource(session, family, model, address, name, attributes):
+    """
+    :param CloudShellAPISession session:
+    :param family:
+    :param model:
+    :param address:
+    :param name:
+    :param attributes:
+    :return:
+    """
+    existing_resource = [r for r in session.GetResourceList().Resources if r.Name == name]
+    if existing_resource:
+        session.DeleteResource(existing_resource[0].Name)
+    resource = session.CreateResource(resourceFamily=family,
+                                      resourceModel=model,
+                                      resourceName=name,
+                                      resourceAddress=address,
+                                      folderFullPath='',
+                                      parentResourceFullPath='',
+                                      resourceDescription='should be removed after test')
+    session.UpdateResourceDriver(resource.Name, model)
+    session.SetAttributesValues(ResourceAttributesUpdateRequest(resource.Name, attributes))
+    return resource
 
 
 def create_command_context_2g(session, ports, controller, attributes):
@@ -86,7 +220,7 @@ def create_command_context_2g(session, ports, controller, attributes):
                                                      durationInMinutes=60)
     reservation_id = reservation.Reservation.Id
 
-    session.AddResourcesToReservation(reservationId=reservation_id, resourcesFullPath=ports)
+    # session.AddResourcesToReservation(reservationId=reservation_id, resourcesFullPath=ports)
 
     shell_attributes = attributes if type(attributes) is list else []
     session.AddServiceToReservation(reservationId=reservation_id,
@@ -110,21 +244,19 @@ def create_command_context_2g(session, ports, controller, attributes):
     return context
 
 
-def create_autoload_resource_2g(session, model, address, name, attributes):
-    resource = session.CreateResource(resourceFamily='CS_TrafficGeneratorChassis',
-                                      resourceModel=model,
-                                      resourceName=name,
-                                      resourceAddress=address,
-                                      folderFullPath='',
-                                      parentResourceFullPath='',
-                                      resourceDescription='should be removed after test')
-    session.UpdateResourceDriver(resource.Name, model)
-    session.SetAttributesValues(ResourceAttributesUpdateRequest(resource.Name, attributes))
-    return resource
+def debug_attach_from_deployment(reservation_id, resource_name=None, service_name=None):
+
+    host, username, password, domain = get_credentials_from_deployment()
+    attach_to_cloudshell_as(
+        server_address=host,
+        user=username,
+        password=password,
+        reservation_id=reservation_id,
+        domain=domain,
+        resource_name=resource_name,
+        service_name=service_name
+    )
 
 
-def end_reservation(session, reservation_id):
-    session.EndReservation(reservation_id)
-    while session.GetReservationDetails(reservation_id).ReservationDescription.Status != 'Completed':
-        time.sleep(1)
-    session.DeleteReservation(reservation_id)
+# Should I move to pylgi? Should I create a general test package that can be extended?
+cisco_mac = {'mac': '48d3.4373.32eb', 'ip': ('10.15.91.43','EB/48D3437332EB')}
